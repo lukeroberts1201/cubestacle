eed32150a7c53d09ba5e5c69ba97646c38bd9c64
LUKE-PC\luker <lukeroberts1201@gmail.com>
2017-09-17 16:42:03 +0100

    Working level select

--- Game_01/Assets/Scripts/LevelComplete.cs
+++ Game_01/Assets/Scripts/LevelComplete.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour {
 
+    public string nextLevel = "Level02";
+    public int levelToUnlock = 2;
+
     public void LoadNextLevel()
     {
+        
+        PlayerPrefs.SetInt("levelReached", levelToUnlock);
+
         SceneManager.LoadScene("LevelSelect");
     }
 }
