be32df68a078fae0c9c75c968fef590d04265359
LUKE-PC\luker <lukeroberts1201@gmail.com>
2017-09-18 18:16:29 +0100

    Added some levels

--- Game_01/Assets/Scripts/LevelComplete.cs
+++ Game_01/Assets/Scripts/LevelComplete.cs
@@ -3,8 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour {
 
-    public string nextLevel = "Level02";
-    public int levelToUnlock = 2;
+    public string nextLevel = "Level03";
+    public int levelToUnlock = 3;
 
     public void LoadNextLevel()
     {
